69067de8d35b6b183f87174a7433fcef9afe702c
murdoch24 <murdoch24@gmail.com>
2026-08-15 12:37:39 +0100

    Code cleanup

--- main.py
+++ main.py
@@ -27,11 +27,12 @@ for index, row in df.iterrows():
     pdf.set_text_color(180,180,180)
     pdf.cell(w=0, h=10, txt=row["Topic"], align="R")
 
+    # Add a number of pages as stated in the CSV file
     for i in range(row["Pages"]-1):
         pdf.add_page()
 
-        for i in range(32, 265, 10):
-            pdf.line(10, i, 200, i)
+        for num in range(32, 265, 10):
+            pdf.line(10, num, 200, num)
 
         # Set the Footer
         pdf.ln(277)
