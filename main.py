28abd8141a294e8ce011bfcdacf370cd01776778
murdoch24 <murdoch24@gmail.com>
2026-08-15 12:31:30 +0100

    Add lines on pages to create lined notebook

--- main.py
+++ main.py
@@ -16,14 +16,22 @@ for index, row in df.iterrows():
     pdf.cell(w=0,h=12,txt=row["Topic"], align="L", ln=1)
     pdf.line(10,22,200,22)
 
+    # Create lines on page
+
+    for i in range(32,265,10):
+        pdf.line(10, i, 200, i)
+
     # Set the Footer
-    pdf.ln(265)
+    pdf.ln(275)
     pdf.set_font(family="Times", style="I", size=8)
     pdf.set_text_color(180,180,180)
     pdf.cell(w=0, h=10, txt=row["Topic"], align="R")
 
     for i in range(row["Pages"]-1):
         pdf.add_page()
+
+        for i in range(32, 265, 10):
+            pdf.line(10, i, 200, i)
 
         # Set the Footer
         pdf.ln(277)
